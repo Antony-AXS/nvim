3404841a6b5d1497d35c5f537ee7c8ec1bbc88f3
Antony Xavier <antonyaxs324@gmail.com>
2025-04-30 19:43:01 +0530

    mason-null-ls.nvim plugin added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -47,6 +47,17 @@ return {
 		event = "VeryLazy",
 	},
 	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("your.null-ls.config") -- require your null-ls config here (example below)
+		end,
+	},
+	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	{
